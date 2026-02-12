365133bf532486fdafff03218865bf5160bedc25
Tomas Robino <tomaserobino@gmail.com>
2026-02-12 10:38:59 +0100

    mongoutil

--- src/main/java/org/example/MongoUtil.java
+++ src/main/java/org/example/MongoUtil.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 public class MongoUtil {
+    private static MongoClient client;
+    private static MongoDatabase db;
+
+    public static MongoDatabase getDatabase() {
+        if (db == null) {
+            client = MongoClients.create("mongodb://localhost:27017");
+            db = client.getDatabase("centro_fp");
+        }
+        return db;
+    }
+
+    public static void close() {
+        if (client != null) client.close();
+    }
 }
