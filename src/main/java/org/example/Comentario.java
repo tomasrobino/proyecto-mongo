239a6da47d00d6d3022d2685d064a56356c80ee8
Tomas Robino <tomaserobino@gmail.com>
2026-02-12 10:52:18 +0100

    campos modelos

--- src/main/java/org/example/Comentario.java
+++ src/main/java/org/example/Comentario.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class Comentario {
+    private String contenido;
+
+    public Comentario() {}
 }
--- src/main/java/org/example/Post.java
+++ src/main/java/org/example/Post.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import java.util.Date;
+import java.util.List;
+
 public class Post {
+    private String titulo;
+    private String mensaje;
+    private String etiquetas;
+    private Date fechaPublicacion;
+    private List<Comentario> comentarios;
+
+    public Post() {}
 }
